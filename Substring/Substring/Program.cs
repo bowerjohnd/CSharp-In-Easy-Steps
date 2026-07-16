6075b224e46f3c967e65ebfe4dbe7c8ae787f266
bowerjohnd <bowerjohnd@gmail.com>
2026-07-16 14:24:38 -0400

    Chap 6 projects and clean up untracked Visual Studio files

---
+++ Substring/Substring/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Title = "Substring";
+
+static void report(int pos, string sub)
+{
+    if (pos != -1)
+    {
+        Console.WriteLine("'" + sub + "' Found At " + pos);
+    }
+    else
+    {
+        Console.WriteLine("'" + sub + "' Not Found!");
+    }
+}
+
+string text = "My dog is a cute dog";
+Console.WriteLine(text + "\tLength: " + text.Length);
+
+Console.WriteLine("\nPlease Enter A Substring To Seek: ");
+string sub = Console.ReadLine();
+
+char[] arr = new char[sub.Length];
+sub.CopyTo(0, arr, 0, sub.Length);
+
+int pos = text.IndexOf(sub);
+report(pos, sub);
+
+ pos = text.LastIndexOf(sub);
+report(pos, sub);
+
+pos = text.IndexOfAny(arr);
+report(pos, text.Substring(pos, 1));
+
+pos = text.LastIndexOfAny(arr);
+report(pos, text.Substring(pos, 1));
+
+Console.ReadKey();
