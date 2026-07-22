a5a0b1ae9e23ae09b4b8827774ac74f4c4f1cd23
bowerjohnd <bowerjohnd@gmail.com>
2026-07-22 14:53:03 -0400

    Chapter 8

---
+++ Breakpoints/Breakpoints/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Title = "Breakpoints";
+
+int i, j, k;
+int pass = 0;
+
+for ( i = 1; i < 4; i++ )
+{
+    for ( j = 1; j < 4; j++ )
+    {
+        for ( k = 1; k < 4; k++ )
+        {
+            pass++;
+        }
+    }
+}
---
+++ ErrorHandling/ErrorHandling/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Title = "ErrorHandling";
+
+Console.Write("Please Enter A Number: ");
+try
+{
+	double num1 = Convert.ToInt16(Console.ReadLine());
+
+	Console.Write("Now Enter Another Number: ");
+	double num2 = Convert.ToInt16(Console.ReadLine());
+
+	Console.WriteLine("Total: " + (num1 + num2));
+}
+//catch (OverflowException)
+//{
+//	Console.WriteLine("\nMaximum: " + Int16.MaxValue);
+//}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message + "\n" + error.GetType());
+}
